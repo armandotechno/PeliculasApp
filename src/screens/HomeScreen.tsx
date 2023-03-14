2d9bb321f144567ee4aefeb36dee695bc90b221f
Armando Capriles <armandotechno12@gmail.com>
2023-03-13 20:50:32 -0400

    Helper para obtener los colores de las imagenes

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -8,6 +8,7 @@ import { GradientBackground } from '../components/GradientBackground';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { MoviePoster } from '../components/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
+import { getImageColors } from '../helpers/getColores';
 
 const { width: windowWidth } = Dimensions.get('window');
 
@@ -20,9 +21,9 @@ export const HomeScreen = () => {
       const movie = nowPlaying[index];
       const uri = `https://image.tmdb.org/t/p/w500${ movie.poster_path }`   
       
-      const colors = await ImageColors.getColors( uri, {})
-      
-      console.log(colors);
+      const [ primary, secondary ] = await getImageColors( uri );
+
+      console.log({ primary, secondary });
       
       
     }
