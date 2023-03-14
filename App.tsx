134d6c201f9e06ef09a232eb6ec61dcb798453d2
Armando Capriles <armandotechno12@gmail.com>
2023-03-13 21:16:48 -0400

    Creando un context

--- App.tsx
+++ App.tsx
@@ -2,12 +2,24 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navegation } from './src/navegation/Navegation';
 import { FadeScreen } from './src/screens/FadeScreen';
+import { GradientProvider } from './src/context/GradientContext';
+
+const AppState = ({ children }: any ) => {
+  
+  return (
+    <GradientProvider>
+      { children }
+    </GradientProvider>
+  )
+}
 
 const App = () => {
   return (
     <NavigationContainer>
-        <Navegation />
-        {/* <FadeScreen /> */}
+        <AppState>
+          <Navegation />
+          {/* <FadeScreen /> */}
+        </AppState>
     </NavigationContainer>
   )
 }
